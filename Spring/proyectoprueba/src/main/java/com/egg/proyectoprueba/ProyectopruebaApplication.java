7bef512d84e79b54fcbf1d6ab58552622ae65525
Luciana E Urristi <129803372+LucianaUrristi@users.noreply.github.com>
2024-10-29 19:20:11 -0300

    Agrego ejercicio "proyecto prueba"

---
+++ Spring/proyectoprueba/src/main/java/com/egg/proyectoprueba/ProyectopruebaApplication.java
@@ -0,0 +1,13 @@
+package com.egg.proyectoprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectopruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectopruebaApplication.class, args);
+	}
+
+}
---
+++ Spring/proyectoprueba/src/test/java/com/egg/proyectoprueba/ProyectopruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.proyectoprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
